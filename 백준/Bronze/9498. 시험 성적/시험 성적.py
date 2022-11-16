89b069ddb4725f416942119d6bd0700e908c6133
jueunx0x <67046550+jueunx0x@users.noreply.github.com>
2022-11-16 22:56:13 +0900

    [Bronze V] Title: 시험 성적, Time: 80 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,13 @@
+A=int(input())
+if A < 0 or A > 100:
+    A = int(input())
+if A>=90 and A<=100:
+    print("A")
+elif A>=80 and A<90:
+    print("B")
+elif A>=70 and A<80:
+    print("C")
+elif A>=60 and A<70:
+    print("D")
+else:
+    print("F")
