432f80db56bde1a84a4da5e6c2b105d5b66e6988
jueunx0x <67046550+jueunx0x@users.noreply.github.com>
2023-08-21 21:40:38 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 912 ms, Memory: 237052 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+import java.io.BufferedWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= T; i++) {
+
+            // Press Ctrl+D to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Cmd+F8.
+            st = new StringTokenizer(br.readLine());
+            bw.write((Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()))+"\n");
+        }
+        bw.close();
+    }
+}
