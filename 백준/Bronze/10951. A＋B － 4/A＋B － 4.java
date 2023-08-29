5d40acb9a419dbc0005d1a364f8e3c6f4504de72
jueunx0x <67046550+jueunx0x@users.noreply.github.com>
2023-08-29 21:18:28 +0900

    [Bronze V] Title: A+B - 4, Time: 236 ms, Memory: 17884 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int A,B;
+
+        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+        while (sc.hasNext()){
+            A = sc.nextInt();
+            B = sc.nextInt();
+            System.out.println(A+B);}
+        sc.close();
+
+    }
+
+    }
+
