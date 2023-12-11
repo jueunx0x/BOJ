8b065407a42039c57897a8d4d7ffefc67c378137
jueunx0x <67046550+jueunx0x@users.noreply.github.com>
2023-12-11 23:09:28 +0900

    [Bronze V] Title: A+B - 2, Time: 208 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        sc.close();
+        int sum=0;
+        sum = A+B;
+        System.out.println(sum);
+        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+
+
+    }
+
+}
